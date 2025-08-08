7117fd735ff851b833aa2ee5dec627f335a98536
DON-499 <chisimdiekube7@gmail.com>
2025-08-08 02:14:18 -0700

    Add files via upload

---
+++ project.js
@@ -0,0 +1,9 @@
+function togglemenu(){
+        if(menulist.style.maxHeight=="0px")
+        {
+            menulist.style.maxHeight="130px"
+        }
+        else{
+            menulist.style.maxHeight="0px"
+        }
+    }
